79de091b3151b5d6a16885d0391eab5708bc53de
gakerig1 <106721693+gakerig1@users.noreply.github.com>
2022-06-03 18:30:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['62881036447529','6281779521531','6287789137285']
+global.premium = ['6287789137285']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
